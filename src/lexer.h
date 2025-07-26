8df40f1428edd2c4d11e41216c28c1f7413f5f7c
vinajani <vinajani@student.21-school.ru>
2025-07-27 02:41:07 +0500

    update 1
    clang-format 		для lexer.c lexer.h
    add 			debug_lexer.c
    
    build debug_lexer 	gcc -Wall -Wextra -Werror -std=c11 debug_lexer.c lexer.c -o ../build/debug_lexer

--- src/lexer.h
+++ src/lexer.h
@@ -1,15 +1,24 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-typedef enum { T_NUMBER, T_OPERATOR, T_LPAREN, T_RPAREN, T_VARIABLE, T_FUNCTION } TokenType;
+typedef enum
+{
+    T_NUMBER,
+    T_OPERATOR,
+    T_LPAREN,
+    T_RPAREN,
+    T_VARIABLE,
+    T_FUNCTION
+} TokenType;
 
-typedef struct {
+typedef struct
+{
     TokenType type;
     char str[16];
     double value;
 } Token;
 
 //// Возвращает количество токенов или -1 (при ошибке)
-int tokenize(const char *expression, Token *tokens, int max_tokens);
+int tokenize(const char *expr, Token *out, int cap);
 
 #endif
